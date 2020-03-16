d9d26f1d6d9307d2da9f18be99116ee456cfc9b5
josecaos <jazzvoon@gmail.com>
2020-03-16 13:41:27 -0600

    Valida captcha

--- src/componentes/Fomulario.svelte
+++ src/componentes/Fomulario.svelte
@@ -1,14 +1,30 @@
 <script>
-  const verifica = () => {
-    console.log("Verifica");
+
+  const siteKey = "6Lc_s-EUAAAAACWS7IpdI5nnh4krvflPvM00ah8l";
+
+  let respuesta;
+
+  const valida = e => {
+    let nombre = e.target[0].value;
+    let correo = e.target[1].value;
+    let mensaje = e.target[2].value;
+    let captcha = e.target[3].value;
+
+    if (!!captcha) {
+    //   console.log("Valida", nombre, correo, mensaje, captcha);
+      respuesta.style.color = "green";
+      respuesta.innerHTML = "Se han enviado los datos";
+    } else {
+        //   console.log("No se ha contestado el captcha");
+      respuesta.style.color = "red";
+      respuesta.innerHTML = "No se ha contestado el captcha";
+    }
   };
-  const valida = () => {
-    console.log("Valida");
-  };
+
 </script>
 
 <h1>Formulario con recaptcha</h1>
-<form on:submit={valida()}>
+<form on:submit|preventDefault={valida}>
 
   <label>
     Nombre:
@@ -23,9 +39,10 @@
     <textarea name="mensaje" id="mensaje" />
   </label>
 
-  <div class="g-recaptcha" data-callback="verifica" data-sitekey="6Lc_s-EUAAAAACWS7IpdI5nnh4krvflPvM00ah8l" />
+  <div class="g-recaptcha" data-sitekey={siteKey} />
 
   <div>
     <button type="submit">Enviar</button>
   </div>
+  <p class="respuesta" bind:this={respuesta} />
 </form>
